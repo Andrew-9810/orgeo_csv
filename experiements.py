bb1862f5152d0a4c83a531c5d63e78aabb93ced8
Golovanov Andrey <andrew-9810@yandex.ru>
2025-06-24 17:12:13 +0300

    version 2 test

--- experiements.py
+++ experiements.py
@@ -34,28 +34,80 @@
 
 from decimal import *
 
-comments = [
-    'C:23', 'C:2', 'C:234', 'C:3', 'C:34', 'C:1',
-    'C:1234', 'C:12', 'C:123', 'C:134', 'C:14'
-]
-start = 4
-count_firlds = 2 + start + 4
-
-comment_template = []
-while count_firlds != 0:
-    """Подготовка позиций в комментарии. Создает строку с пробелами."""
-    comment_template.append(' ')
-    count_firlds -= 1
-
-comment_output = []
-
-for comment in comments:
-    comment_output = comment_template[:]
-    poss = list(comment.lstrip('C:'))
-    comment_output[0], comment_output[1] = 'C', ':'
-    for i in poss:
-        comment_output[int(i) - 1 + 2] = i
-    print(comment_output)
 
 
+# comments = [
+#     'C:23', 'C:2', 'C:234', 'C:3', 'C:34', 'C:1',
+#     'C:1234', 'C:12', 'C:123', 'C:134', 'C:14'
+# ]
+# start = 4
+# count_firlds = 2 + start + 4
+#
+# comment_template = []
+# while count_firlds != 0:
+#     """Подготовка позиций в комментарии. Создает строку с пробелами."""
+#     comment_template.append(' ')
+#     count_firlds -= 1
+#
+# comment_output = []
+#
+# for comment in comments:
+#     comment_output = comment_template[:]
+#     poss = list(comment.lstrip('C:'))
+#     comment_output[0], comment_output[1] = 'C', ':'
+#     for i in poss:
+#         comment_output[int(i) - 1 + 2] = i
+#     print(comment_output)
 
+
+
+# csv = ['red', 'blue', 'green', 'white']
+# json = ['red', 'white', 'цвет']
+# def cech(list1, list2):
+#     """list1- list2- """
+#     x = list1
+#     for i in list2:
+#         if i in x:
+#             print(i)
+#         else:
+#             print(f'{i} no')
+#
+# cech(csv, json)
+# print('='*20)
+# cech(json, csv)
+
+
+# x = [9, 10, 11, 12]
+# for i in x:
+#     if i in  [10, 11]:
+#         print(i, 10, 11)
+#     else:
+#         print(i)
+
+class A:
+    def __init__(self):
+        self.value = 'marmelad'
+        self.value1 = 3
+
+    def __repr__(self):
+        return f' repr {self.value, self.value1}'
+    def __str__(self):
+        return f'{self.value, self.value1}'
+f = A()
+print(f)
+class B:
+    def __init__(self):
+        self.lolalola = []
+        for i in range(4):
+            # print(i)
+            x = A()
+            self.lolalola.append(x)
+
+    def fu(self):
+        # print(self.lolalola)
+        return self.lolalola
+
+b = B()
+b.fu()
+print(b.lolalola[0])
+# print(b.lolalola)
--- main.py
+++ main.py
@@ -4,24 +4,83 @@ import csv
 import pathlib
 import logging
 
+
 logging.basicConfig(level=logging.INFO, filename='py_log.log',filemode='w')
 
+def compare(list1, list2):
+    """list1- list2- """
+    result = []
+    x = list1
+    for i in list2:
+        if i in x:
+            pass
+        else:
+            print(i)
+            result.append(i)
+    return result
+
+
+class Comment:
+    def __init__(self, symbol, position, flag_new):
+        self.symbol = symbol
+        self.position = position
+        self.flag_new = flag_new
+
+
+class FildComment:
+    def __init__(self, path):
+        self.path = path
+        self.file = self.input_output()
+        self.start = self.file['start']
+        if self.start <= 0:
+            logging.info("Ошибка! Колличество стартов меньше или равно 0")
+            exit()
+        self.position = {}
+        for key_json in self.file['switch']:
+            comment = self.add_pos(self.file['switch'][key_json])
+            self.position[key_json] = comment
+
+    def input_output(self, flag='r'):
+        """Загрузка выгрузка"""
+        if flag == 'r':
+            with open(self.path, 'r', encoding='utf-8') as f:
+                """Файл json c переключателями."""
+                file_content = f.read()
+            return json.loads(file_content)
+        elif flag == 'w':
+            file = {'start':self.start, 'switch':{}}
+            for key in self.position:
+                data = self.position.get(key)
+                file['switch'][key] = [data.symbol, data.position, data.flag_new]
+            with open(self.path, 'w', encoding='utf-8') as outfile:
+                json.dump(file, outfile, ensure_ascii=False)
+
+    def add_pos(self, data):
+        """Добавление позиции."""
+        return Comment(data[0], data[1], data[2])
+
+    def edit_pos(self):
+        """Изменение позиции."""
+        pass
+
+    def del_pos(self):
+        """Удаление позиции."""
+        pass
+
 # Пути к файлам
-input_file = pathlib.Path('input_file/34694_csv_wo_predstavitel_2024_05_15.csv')
+input_file = pathlib.Path('input_file/40198_csv_wo_predstavitel_2025_06_18.csv')
 json_file = pathlib.Path('fields.json')
 
 booking_poss = 2  # C:
 
 export_orgeo = []
 file_base = []
-comment_template = []
+template_com = []
 
-with open(json_file) as json_f:
-    """Файл json c переключателями."""
-    file_content = json_f.read()
-    fields_switch = json.loads(file_content)
 
-logging.info(f'fields_switch {fields_switch}')
+com = FildComment(json_file)
+
+
 
 with open(input_file, encoding='windows-1251') as csv_f:
     """Файл csv c выгрузкой из orgeo."""
@@ -29,61 +88,74 @@ with open(input_file, encoding='windows-1251') as csv_f:
     for line in csv_file:
         export_orgeo.append(line)
 
-count_firlds = 0
+count_firlds = 20 # Кол-во символов WinOrient в комменте
 
-"""
-Подсчет колличества позиций, находит самую большую позицию
-"""
-# Ошибка на ноль
-if fields_switch['start'] <= 0:
-    logging.info("Ошибка! Колличество стартов меньше или равно 0")
-else:
-    for field in fields_switch['switch']:
-        count = fields_switch['switch'][field][1]
-        if count > count_firlds:
-            count_firlds = count
+sets = set() # Показатели orgeo
+for row in export_orgeo:
+    # Забрал все показатели из csv
+    for i in row[8].split(',')[1:]: # Убрал определение дней
+        sets.add(i.strip())
 
-logging.info(f'count_firlds {count_firlds}')
+
+keys_json = com.position.keys()
+
+
+print('__Новые__')
+new = compare(list(keys_json), list(sets))
+print('___Невостребованные___')
+old = compare(list(sets), list(keys_json))
+
+def func(x):
+    com.position[x] = com.add_pos(['', 0, True])
+
+if len(new) > 1:
+    for pos in new:
+        func(pos)
+elif len(new) == 1:
+    func(new[0])
+
+com.input_output('w')
+
 
 while count_firlds != 0:
     """Подготовка позиций в комментарии. Создает строку с пробелами."""
-    comment_template.append(' ')
+    template_com.append('_')
     count_firlds -= 1
+
+# input("Проверь показатели, внеси новые и нажми Enter")
 
 # Работа с комментарием
 for row in export_orgeo:
-    comment_output = comment_template[:]
-    # C:23, 800, Оплачено, C:23,800,Оплачено,
-    comment = row[8].split(',')
-    # Работа с комментарием блок день
-    if fields_switch['start'] == 1:  # Однодневный старт.
-        comment_output[0], comment_output[1], comment_output[2] = 'C', ':', '1'
+    comment_output = template_com[:]
+    print(comment_output)
+    # Работа с комментарием блок день позиции с 4 по 9 (-1)
+    running_day = 0
+    if com.start == 1:  # Однодневный старт.
+        comment_output[3] = '1'
+        running_day = 1
     else:
         # Нулевая позиция коментария равна С:1234
-        for block_day in comment[0]:
-            poss = list(block_day.lstrip('C:'))
-            comment_output[0], comment_output[1] = 'C', ':'
-            for i in poss:
-                comment_output[int(i) - 1 + booking_poss] = i
-
-    for field in fields_switch['switch']:
-
-        if field in comment:
-            comment_volume = fields_switch['switch'][field][0]
-            comment_position = fields_switch['switch'][field][1]
-            # list начинаетсяс нулевой позиции
-            comment_output[comment_position - 1] = comment_volume
-        else:
-            continue
-    # Конвертация list в строку
-    comment_str = ''
-    for i in comment_output:
-        comment_str += f'{i}'
-    row[9] = comment_str
-
+        comment = row[8].split(',')[:1]
+        block_day = ''.join(comment)
+        for day in range(1, com.start + 1):
+            if str(day) in block_day:
+                comment_output[day + 2] = str(day)
+                running_day += 1
+    # Работа с показателями
+    for pos in com.position:
+        if pos in row[8]:
+            if com.position[pos].position in [10, 11]:
+                comm_symbol = str(running_day)
+            else:
+                comm_symbol = com.position[pos].symbol
+            comment_output[com.position[pos].position - 1] = comm_symbol
+    result = ''.join(comment_output)
+    print(result)
+    row.append(result)
+    # меняем местами
     row[8], row[9] = row[9], row[8]
-    logging.info(row)
     file_base.append(row)
+
 
 with open('output_file/import_orgeo.csv', encoding='windows-1251', mode='w') as imp_org:
     spamwriter = csv.writer(imp_org, delimiter=';')
